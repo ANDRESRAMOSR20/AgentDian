c1b04f59c5025ac06354aae23b58bf75ba9ebb15
AndresRamos <andresydana611@gmail.com>
2025-02-04 12:48:28 -0500

    RAG started

--- src/confg.py
+++ src/confg.py
@@ -1,8 +1,8 @@
 from langchain_ollama import ChatOllama
-from langchain_ollama import OllamaEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 
 
-embeddings = OllamaEmbeddings(model="llama3")
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 llm = ChatOllama(model="llama3.2:1b")
 vector_store = Chroma(embedding_function=embeddings)
